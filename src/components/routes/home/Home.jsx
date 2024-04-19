e10476b87af97682d2a20f9354c434f00f7cf067
sylvain-galoustoff <146923279+Sylvain-MED@users.noreply.github.com>
2024-04-19 16:07:59 +0200

    Home : dynamic cards ok

--- src/components/routes/home/Home.jsx
+++ src/components/routes/home/Home.jsx
@@ -1,13 +1,16 @@
 import LogementCard from "./LogementCard";
+import data from "../../../data/data";
 
 function Home() {
+  console.log(data);
+
+  const renderCards = data.map((item) => (
+    <LogementCard key={item.id} data={item} />
+  ));
+
   return (
     <div className="page container" id="home">
-      <LogementCard />
-      <LogementCard />
-      <LogementCard />
-      <LogementCard />
-      <LogementCard />
+      {renderCards}
     </div>
   );
 }
--- src/components/routes/home/LogementCard.jsx
+++ src/components/routes/home/LogementCard.jsx
@@ -1,11 +1,18 @@
-function LogementCard() {
+import PropTypes from "prop-types";
+
+function LogementCard({ data }) {
   return (
     <div className="card">
+      <img src={data.cover} alt={data.title} />
       <div className="layer">
-        <h2 className="card-title">Titre de la location</h2>
+        <h2 className="card-title">{data.title}</h2>
       </div>
     </div>
   );
 }
+
+LogementCard.propTypes = {
+  data: PropTypes.object,
+};
 
 export default LogementCard;
